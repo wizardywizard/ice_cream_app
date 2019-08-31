34a599020ed9637f5cdd0a4cceec78fc56e605fb
wizardywizard <littledavid175@gmail.com>
2019-08-31 10:39:28 -0700

    inital commit

---
+++ lib/ice_cream_app.rb
@@ -0,0 +1,6 @@
+require "ice_cream_app/version"
+
+module IceCreamApp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ice_cream_app/version.rb
@@ -0,0 +1,3 @@
+module IceCreamApp
+  VERSION = "0.1.0"
+end
