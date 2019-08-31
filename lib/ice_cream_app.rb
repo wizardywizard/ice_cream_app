15eed337652f63faa4b30d3134269ea53b55b4a8
wizardywizard <littledavid175@gmail.com>
2019-08-31 12:41:14 -0700

    third commit

--- lib/ice_cream_app.rb
+++ lib/ice_cream_app.rb
@@ -1,4 +1,5 @@
 require "ice_cream_app/version"
+require "ice_cream_app/ice_cream"
 
 module IceCreamApp
   class Error < StandardError; end
--- lib/ice_cream_app/ice_cream.rb
+++ lib/ice_cream_app/ice_cream.rb
@@ -1,15 +1,27 @@
 require 'open-uri'
 require 'pry'
+require "nokogiri"
 
 class Scraper
 
-    def self.scrape_index_page(https://www.haagendazs.us/products/ice-cream)
-        ice_cream = []
-        doc = Nokogiri::HTML(open(https://www.haagendazs.us/products/ice-cream))
-        doc.css("div.name-product h2").each do |cream|
-            ice_cream[:flavor] = cream.css("span")
-            binding.pry
+    def self.scrape_index_page
+        ice_cream_flavors = []
+        doc = Nokogiri::HTML(open("https://www.haagendazs.us/products/ice-cream"))
+        doc.css("div.name-product h2").map do |cream|
+            flavor = cream.text.strip
+            ice_cream_flavors << {:flavor => "#{flavor}"}
         end
+        ice_cream_flavors
+    end
+
+    def self.scrape_flavor_page(flavor_url)
+        rating = []
+        binding.pry
+        doc.css("div.bv_avgRating_component_container").map do |star|
+            rate = star.itemprop.strip
+            rating << {:rate => "#{rate}"}
+        end
+        rating
     end
 
 end
