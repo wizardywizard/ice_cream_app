863af2a66033120c8b96904ae8cdc41ad7a4ccc2
wizardywizard <littledavid175@gmail.com>
2019-09-09 10:14:25 -0700

    creat objects

--- lib/ice_cream_app/ice_cream.rb
+++ lib/ice_cream_app/ice_cream.rb
@@ -12,4 +12,21 @@ class Ice_cream
         @@all << self
     end
 
+    def self.create_from_collection(ice_cream_array)
+        ice_cream_array.each do |cream|
+            Ice_cream.new(cream)
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+    def all_rates(rates_array)
+        rates_array.each do |key, value|
+            self.send(("#{key}="), value)
+        end
+    end
+
 end
+
