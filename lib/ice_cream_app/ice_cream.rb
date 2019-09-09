50987c59aa6c5372993e14efb3eec92309e2cd10
wizardywizard <littledavid175@gmail.com>
2019-09-09 10:13:00 -0700

    late commit

--- lib/ice_cream_app/ice_cream.rb
+++ lib/ice_cream_app/ice_cream.rb
@@ -1,27 +1,15 @@
-require 'open-uri'
 require 'pry'
-require "nokogiri"
 
-class Scraper
+class Ice_cream
 
-    def self.scrape_index_page
-        ice_cream_flavors = []
-        doc = Nokogiri::HTML(open("https://www.haagendazs.us/products/ice-cream"))
-        doc.css("div.name-product h2").map do |cream|
-            flavor = cream.text.strip
-            ice_cream_flavors << {:flavor => "#{flavor}"}
-        end
-        ice_cream_flavors
-    end
+    attr_accessor :flavor , :rating
 
-    def self.scrape_flavor_page(flavor_url)
-        rating = []
-        binding.pry
-        doc.css("div.bv_avgRating_component_container").map do |star|
-            rate = star.itemprop.strip
-            rating << {:rate => "#{rate}"}
-        end
-        rating
+    @@all = []
+
+    def initialize(ice_cream_hash)
+        @flavor = flavor
+        @rating = rating
+        @@all << self
     end
 
 end
